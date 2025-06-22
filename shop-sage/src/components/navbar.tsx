c31ed6eab8c9ec29780f23c39a69bc6ab0f8d71f
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-06-22 20:20:26 +0530

    feat: added basic navbar

--- shop-sage/src/components/navbar.tsx
+++ shop-sage/src/components/navbar.tsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex justify-between items-center p-3 shadow-md ">
+      <h1 className="text-2xl font-bold ">ShopSage</h1>
+      <input type="text" placeholder="Search..." className="border border-gray-300 rounded-lg p-1 w-1/2"/>
+      <ul className="flex gap-2">
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/cart"><li>Cart</li></Link>
+        <Link to="/wishlist"><li>Wishlist</li></Link>
+      </ul>
+    </nav>
   )
 }
 
