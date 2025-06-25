53d29e71d7972f3261934adc0a592552b7daebfb
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-06-25 22:23:32 +0530

    enhanced search with debounce from lodash for optimized api calls

--- shop-sage/src/components/search.tsx
+++ shop-sage/src/components/search.tsx
@@ -1,10 +1,12 @@
+import { debounce } from "lodash";
 const Search = ({ setSearch }: any) => {
+  const debouncedSearch = debounce((value: string) => setSearch(value), 500);
   return (
     <input
       type="text"
       placeholder="Search..."
       className="border border-gray-300 rounded-lg p-1 w-1/2"
-      onChange={(e) => setSearch(e.target.value)}
+      onChange={(e) => debouncedSearch(e.target.value)}
     />
   );
 };
